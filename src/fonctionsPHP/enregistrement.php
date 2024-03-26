03f72dcf92414c592bcccfcb8fdd40b868e3730f
Frizoks <luc.lecarpentier5@gmail.com>
2024-03-26 08:51:43 +0100

    re

--- src/fonctionsPHP/enregistrement.php
+++ src/fonctionsPHP/enregistrement.php
@@ -8,29 +8,29 @@ $password = "Miss10sur10!";
 
 // Connexion à la base de données PostgreSQL
 try {
-    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
-    // Définir le mode d'erreur PDO sur Exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion à la base de données PostgreSQL réussie.";
+	$conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+	// Définir le mode d'erreur PDO sur Exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connexion à la base de données PostgreSQL réussie.";
 } catch (PDOException $e) {
-    echo "Erreur de connexion à la base de données PostgreSQL : " . $e->getMessage();
+	echo "Erreur de connexion à la base de données PostgreSQL : " . $e->getMessage();
 }
 
 // Vérifier si le formulaire a été soumis et le fichier téléchargé
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
-    // Obtenir les détails sur le fichier téléchargé
-    $fileName = basename($_FILES["file"]["name"]);
-    $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
+	// Obtenir les détails sur le fichier téléchargé
+	$fileName = basename($_FILES["file"]["name"]);
+	$fileType = pathinfo($fileName, PATHINFO_EXTENSION);
 
-    // Vérifier si le fichier est un fichier réel et avec l'extension .xlsx
-    if ($fileType == "xlsx") {
-        // Lire le contenu du fichier Excel
-        require '../../vendor/autoload.php'; // Charge la bibliothèque PhpSpreadsheet
-        $reader = new PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-        $spreadsheet = $reader->load($_FILES["file"]["tmp_name"]);
-        $worksheet = $spreadsheet->getActiveSheet();
-        
-        // Parcourir les lignes du fichier Excel à partir de la deuxième ligne (pour éviter la ligne d'en-tête)
+	// Vérifier si le fichier est un fichier réel et avec l'extension .xlsx
+	if ($fileType == "xlsx") {
+		// Lire le contenu du fichier Excel
+		require '../../vendor/autoload.php'; // Charge la bibliothèque PhpSpreadsheet
+		$reader = new PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+		$spreadsheet = $reader->load($_FILES["file"]["tmp_name"]);
+		$worksheet = $spreadsheet->getActiveSheet();
+		
+		// Parcourir les lignes du fichier Excel à partir de la deuxième ligne (pour éviter la ligne d'en-tête)
 		foreach ($worksheet->getRowIterator() as $row) {
 			// Ignorer la première ligne (en-tête)
 			if ($row->getRowIndex() == 1) {
@@ -48,10 +48,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["file"])) {
 			$stmt->execute($rowData);
 		}
 
-        echo "Les données du fichier $fileName ont été insérées avec succès dans la base de données.";
-    } else {
-        echo "Seuls les fichiers .xlsx sont autorisés.";
-    }
+		echo "Les données du fichier $fileName ont été insérées avec succès dans la base de données.";
+	} else {
+		echo "Seuls les fichiers .xlsx sont autorisés.";
+	}
 }
 
 // Fermer la connexion à la base de données
