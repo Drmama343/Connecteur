e1098e57990587559a99cd18cb978d1728ac873d
Frizoks <luc.lecarpentier5@gmail.com>
2024-04-03 14:02:17 +0200

    dsfgfd

--- src/bado/Fonctions.sql
+++ src/bado/Fonctions.sql
@@ -1,25 +1,18 @@
 CREATE OR REPLACE FUNCTION MoyenneMathsParAnnee(nip_param INT, annee_param VARCHAR)
-RETURNS TABLE (moyenne DECIMAL(10,2), rang BIGINT) AS $$
+RETURNS DECIMAL AS $$
+DECLARE
+	moyenne DECIMAL(10,2);
 BEGIN
-    RETURN QUERY
-    WITH MoyenneEtRang AS (
-        SELECT CAST(AVG(moyress) AS DECIMAL(10,2)) AS moyenne,
-               RANK() OVER (ORDER BY AVG(moyress) DESC) AS rang
-        FROM MoyRess
-        WHERE ((annee_param = 'BUT1' AND idress IN ('BINR106', 'BINR107', 'BINR207', 'BINR208', 'BINR209'))
-            OR (annee_param = 'BUT2' AND idress IN ('BINR308', 'BINR309', 'BINR412'))
-            OR (annee_param = 'BUT3' AND idress IN ('BINR511', 'BINR512')))
-            AND codenip != nip_param -- Exclure l'étudiant actuel de la comparaison
-    )
-    SELECT MoyenneEtRang.moyenne, MoyenneEtRang.rang
-    FROM MoyenneEtRang;
+	SELECT CAST(AVG(moyress) AS DECIMAL(10,2)) INTO moyenne
+	FROM MoyRess
+	WHERE ((annee_param = 'BUT1' AND idress IN ('BINR106', 'BINR107', 'BINR207', 'BINR208', 'BINR209'))
+		OR (annee_param = 'BUT2' AND idress IN ('BINR308', 'BINR309', 'BINR412'))
+		OR (annee_param = 'BUT3' AND idress IN ('BINR511', 'BINR512')))
+		AND codenip = nip_param;
+
+	RETURN moyenne;
 END;
 $$ LANGUAGE plpgsql;
-
-
-
-
-
 
 
 CREATE OR REPLACE FUNCTION MoyenneAnglaisParAnnee(nip_param INT, annee_param VARCHAR)
